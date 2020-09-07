90ed026067765f934b42907fc8ce010101ddcad8
matata samuel <mattasamuel3@gmail.com>
2020-09-07 07:17:11 +0300

    added password behaviour methods in application

--- password-locker.py
+++ password-locker.py
@@ -80,8 +80,56 @@ def main():
 
     while True:
         print('\n')
-        print("use these short codes: cc - creates a new user, dc - display users, fc -find a user, ex -exit the user list ")
-         
+        print("use these short codes: ca - creates a new user, \n du - display users, \n fc -find a user, \n ex -exit the user list ")
+        print('\n')
+
+        short_code = input().lower()
+
+        if short_code == "ca":
+            print("new_user")
+            print("_"*10)
+
+            print("Enter user name...")
+            user_name = input()           
+            
+            print("Do you want to input your own password or have one generated for you? use short codes\n'gp\n' to generate password.\n \'cyo\' to choose your own password \n \'ex\' to exit...")
+            password_choice = input()
+            password = ''
+
+            if password_choice == "cyo":
+                password = password.join(input("Enter a password of choice........"))
+                # break
+            
+            elif password_choice =="gp":
+                print("enter the lenght of password")
+                pass_len = input()
+                password = password.join(Credential.generate_password(pass_len))
+                # break
+
+            elif password_choice == 'ex':
+                 break
+                    
+            else:
+                print('sorry, i did not get that......please try again!!')
+
+
+            # save new user
+            
+            
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
