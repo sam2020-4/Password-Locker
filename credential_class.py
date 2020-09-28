e0564c5bb8c3c81528907a219e50422f79898ce8
matata samuel <mattasamuel3@gmail.com>
2020-09-28 05:35:22 +0300

    initiated credential functions

--- credential_class.py
+++ credential_class.py
@@ -13,4 +13,13 @@ class Credential:
 	credentials_list =[]
 	user_credentials_list = []
 
-    
+    def __init__(self,username,site_name,account_name,password):
+		'''
+		Method to define the properties for each user object.
+		'''
+
+		self.username = username
+		self.site_name = site_name
+		self.account_name = account_name
+		self.password = password
+
