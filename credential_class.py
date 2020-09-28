5da72372919ee8fd9b849cb5760034a5817f1125
matata samuel <mattasamuel3@gmail.com>
2020-09-28 05:34:17 +0300

    added credential array list

--- credential_class.py
+++ credential_class.py
@@ -3,3 +3,14 @@ import time,sys
 from user_class import User #importing user class 
 import random #import random variable generator
 import string  #import string constants
+
+
+class Credential:
+	'''
+	Class to create  account credentials, generate new passwords and save user information
+	'''
+
+	credentials_list =[]
+	user_credentials_list = []
+
+    
