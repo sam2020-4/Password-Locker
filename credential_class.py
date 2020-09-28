6584fb97e268e5a852b233bacbb5ee824000c579
matata samuel <mattasamuel3@gmail.com>
2020-09-28 05:32:44 +0300

    imported pyperclip to credential class

---
+++ credential_class.py
@@ -0,0 +1,5 @@
+import pyperclip #importing pyperclip for copying to clipboard
+import time,sys
+from user_class import User #importing user class 
+import random #import random variable generator
+import string  #import string constants
